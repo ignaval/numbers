8abe618ffbc513b0d9f06a4743d164f7cd5dfd11
Ignacio <ignavaldi@gmail.com>
2018-05-27 09:55:48 -0300

    Added testnet configuration

--- truffle-config.js
+++ truffle-config.js
@@ -7,6 +7,19 @@ module.exports = {
       port: 9545,
       gas: 5000000,
       network_id: '*'
-    }
+    },
+    ropsten:  {
+    	network_id: 3,
+    	from: "0x897dafd1e0ffae725794343593e8cf6d8de1195b",
+     	host: "localhost",
+     	port:  8545,
+     	gas: 3000000
+      // gasPrice: 10000000000
+		},
+		rinkeby: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 4
+    },
   }
 };
